5f586fdc9be8615d9022563f18a1f9d0ba8936a9
6tab9 <tabolator1@gmail.com>
2025-10-11 13:57:22 +0200

    wasd

--- VMCreator.sh
+++ VMCreator.sh
@@ -2,9 +2,18 @@ if [$# -ne 0];
 then
     echo womp
 else
+    echo "Select a resource group to add machines"
     VMgroups=`az group list|grep '"name":*'|cut -d ":" -f 2`
+    declare -i index=0
     for groupname in $VMgroups
     do
-        echo $groupname
+        echo "$index) $groupname"
+        index+=1
     done
+    echo "Or type a name for a new resource group: "
+    read resourceGroup
+    case $resourceGroup in
+        "[0-9]") echo "`$VMgroups|cut -d "," -f $resourceGroup` has been chosen";;
+        *) echo "$resourceGroup has been created";;
+    esac
 fi
