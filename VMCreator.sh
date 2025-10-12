796bce5bbf92f6d871da09acb46f83825c225182
6tab9 <tabolator1@gmail.com>
2025-10-12 15:35:10 +0200

    ???

--- VMCreator.sh
+++ VMCreator.sh
@@ -17,7 +17,7 @@ else
         [0-$index])
             resourceGroupName=`cut -d '
 ' -f "$resourceGroup" <<< "$VMgroups"|cut -d ',' -f 1`
-	    resourceGroup=$resourceGroupName;;
+	    resourceGroup=`cut --complement -c 2 <<< $resourceGroupName|rev|cut --complement -c 1|rev`;;
         *)
             az group create --location francecentral --resource-group $resourceGroup --only-show-errors
             echo "$resourceGroup has been created";;
@@ -29,10 +29,19 @@ else
     echo "Select type of a machine:"
     printf "1)Windows \n2)Ubuntu\n3)Custom"
     read machineType
-    echo "Input username:"
+    case $machineType in
+        1) machineType="Win2022AzureEditionCore";;
+        2) machineType="Ubuntu2404";;
+        3) echo "TBA";;
+    esac
+    echo "Input vm name: "
+    read vmName
+    echo "Input admin username:"
     read username
+    echo "Input admin password:"
+    read -s passwd
     for i in {1..$amount}
     do
-	
+        az vm create --resource-group $resourceGroup --name $vmName --image $machineType --public-ip-sku Standard --admin-username $username --admin-password $passwd
     done
 fi
